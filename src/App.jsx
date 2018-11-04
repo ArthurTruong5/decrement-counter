478de91248e678b02bd791d9c163166ecf4175f8
arthurtruong <arthurtruong.work@gmail.com>
2018-11-05 00:36:03 +1100

    test

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 10,
+      negative: 0
+    };
+  }
+  render() {
+    const count = this.state.counter;
+    let button;
+    if (count < 0) {
+      button = this.state.negative;
+    } else {
+      button = this.state.counter;
+    }
+
+    return (
+      <div className="App" data-test="component-app">
+        <h1>Button Game</h1>
+        <h2>The counter is currently {button}</h2>
+        <button
+          data-test="decrement-button"
+          onClick={() =>
+            this.setState({
+              counter: this.state.counter - 1
+            })
+          }
+        >
+          Click button
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+// TODO this is very important - shallow
+import Enzyme, { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+Enzyme.configure({ adapter: new Adapter() });
+
+// TODO Create reuseable code
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) wrapper.setState(state);
+  return wrapper;
+};
+
+// Find by test attribute
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
+test("renders without errors", () => {
+  const wrapper = setup();
+  // <App /> = component-app
+  const appComponent = findByTestAttr(wrapper, "component-app");
+  expect(appComponent.length).toBe(1);
 });
+
+test("clicking button decrements counter display"),
+  () => {
+    const counter = 10;
+    const wrapper = setup(null, { counter });
+  };
